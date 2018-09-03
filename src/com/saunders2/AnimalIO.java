af90be8044ecef3b63ca484f0241b161761e9133
Your Name <you@example.com>
2018-09-03 09:02:15 -0500

    moved addX methods to AnimalsIO class

--- src/com/saunders2/AnimalIO.java
+++ src/com/saunders2/AnimalIO.java
@@ -1,5 +1,10 @@
 package com.saunders2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class AnimalIO {
     private String input;
 
@@ -37,5 +42,80 @@ public class AnimalIO {
             System.out.println(e + "is not a boolean");
         }
         return bool;
+    }
+
+    public static Cat addCat() throws IOException {
+        String name = "";
+        int miceKilled = 0;
+
+        BufferedReader inStream = new BufferedReader (new InputStreamReader(System.in));
+
+        System.out.print("Enter your Cat's name: ");
+        try{
+            name = inStream.readLine();
+        } catch(IOException e){
+            System.out.println(e + " is not a string");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of mice killed ");
+        try{
+            miceKilled = scanner.nextInt();
+        }catch (NumberFormatException e){
+            System.out.println(e + " is not an integer");
+        }
+
+        return new Cat(miceKilled, name);
+    }
+
+    public static Dog addDog() throws IOException {
+        String name = "";
+        boolean friendly = true;
+
+        BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter your Dog's's name: ");
+        try {
+            name = inStream.readLine();
+        } catch (IOException e) {
+            System.out.println(e + " is not a string");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("is the dog friendly? ");
+        try {
+            friendly = scanner.nextBoolean();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e + " is not a boolean");
+        }
+
+        return new Dog(friendly, name);
+    }
+
+    public static Student addStudent() throws IOException {
+        String name = "";
+        int age = 0;
+
+        BufferedReader inStream = new BufferedReader (new InputStreamReader(System.in));
+
+        System.out.print("Enter Student name: ");
+        try{
+            name = inStream.readLine();
+        } catch(IOException e){
+            System.out.println(e + " is not a string");
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter student's age");
+        try{
+            age = scanner.nextInt();
+        }catch (NumberFormatException e){
+            System.out.println(e + " is not an integer");
+        }
+
+        return new Student(age, name);
     }
 }
--- src/com/saunders2/Main.java
+++ src/com/saunders2/Main.java
@@ -10,19 +10,19 @@ public class Main {
 
     private final static FileOutput outFile = new FileOutput("animals.txt");
     private final static FileInput inFile = new FileInput("animals.txt");
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ArrayList<Talkable> zoo = new ArrayList<>();
 
         //Lines to Replace
         //zoo.add(new Dog(true, "Pete"));
-
-
         //zoo.add(new Cat(9, "Anne Belly"));
-        zoo.add(new Cat());
-
-        zoo.add(new Student(19, "Joe John Johnson"));
+        //zoo.add(new Student(19, "Joe John Johnson"));
         //End Lines to Replace
+
+        zoo.add(addDog());
+        zoo.add(addCat());
+        zoo.add(addStudent());
 
         for (Talkable thing: zoo) {
             printOut(thing);
@@ -40,80 +40,5 @@ public class Main {
     public static void printOut(Talkable p)  {
         System.out.println(p.getName() + " says=" + p.talk());
         outFile.fileWrite(p.getName() + "|" + p.talk());
-    }
-
-    public static Cat addCat() throws IOException {
-        String name = "";
-        int miceKilled = 0;
-
-        BufferedReader inStream = new BufferedReader (new InputStreamReader(System.in));
-
-        System.out.print("Enter your Cat's name: ");
-        try{
-            name = inStream.readLine();
-        } catch(IOException e){
-            System.out.println(e + " is not a string");
-        }
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number of mice killed ");
-        try{
-            miceKilled = scanner.nextInt();
-        }catch (NumberFormatException e){
-            System.out.println(e + " is not an integer");
-        }
-
-        return new Cat(miceKilled, name);
-    }
-
-    public static Dog addDog() throws IOException {
-        String name = "";
-        boolean friendly = true;
-
-        BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.print("Enter your Dog's's name: ");
-        try {
-            name = inStream.readLine();
-        } catch (IOException e) {
-            System.out.println(e + " is not a string");
-        }
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("is the dog friendly? ");
-        try {
-            friendly = scanner.nextBoolean();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e + " is not a boolean");
-        }
-
-        return new Dog(friendly, name);
-    }
-
-    public static Student addStudent() throws IOException {
-        String name = "";
-        int age = 0;
-
-        BufferedReader inStream = new BufferedReader (new InputStreamReader(System.in));
-
-        System.out.print("Enter your Cat's name: ");
-        try{
-            name = inStream.readLine();
-        } catch(IOException e){
-            System.out.println(e + " is not a string");
-        }
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number of mice killed ");
-        try{
-            age = scanner.nextInt();
-        }catch (NumberFormatException e){
-            System.out.println(e + " is not an integer");
-        }
-
-        return new Student(age, name);
     }
 }
