93247426464969fb150a331853f2d5fce453e438
Your Name <you@example.com>
2018-09-03 09:12:14 -0500

    moved addX methods to AnimalsIO class

--- src/com/saunders2/AnimalIO.java
+++ src/com/saunders2/AnimalIO.java
@@ -12,38 +12,6 @@ public class AnimalIO {
         this.input = input;
     }
 
-    public String getInput() {
-        return input;
-    }
-
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public static Integer convertToInteger(String input) {
-        int num = 0;
-        try {
-            num = Integer.parseInt(input);
-            System.out.println(num + " is an integer");
-
-        }catch (NumberFormatException e) {
-            System.out.println(e + "is not an integer");
-        }
-        return num;
-    }
-
-    public static Boolean convertToBoolean(String input) {
-        boolean bool = false;
-        try {
-            bool = Boolean.parseBoolean(input);
-            System.out.println(bool + " is a boolean");
-
-        }catch (NumberFormatException e) {
-            System.out.println(e + "is not a boolean");
-        }
-        return bool;
-    }
-
     public static Cat addCat() throws IOException {
         String name = "";
         int miceKilled = 0;
